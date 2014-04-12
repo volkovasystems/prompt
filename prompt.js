b53899c210709af0930326c95b36bf92eca62bc6
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2014-04-13 07:04:45 +0800

    Added prompt configuration. Work in progress.

--- prompt.js
+++ prompt.js
@@ -3,12 +3,18 @@ try{ var base = window; }catch( error ){ var base = exports; }
 	define( "prompt",
 		[
 			"argumentsToArray",
+			"hardenAllProperty",
 			"hardenProperty"
 		],
 		function construct( ){
 			var prompt = function prompt( ){
 
 			};
+
+			prompt.configure = function promptConfigure( ){
+
+			};
+			
 			base.prompt = prompt;
 		} );
 } )( base );
